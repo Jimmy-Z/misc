b5c59d95984b083096bd392bc42eb28607aa5907
Jimmy-Z <jimmy.z.z@gmail.com>
2026-08-01 19:24:56 +0800

    init_env_logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
-pub mod pretty;
 pub mod build;
+pub mod pretty;
 
-pub use pretty::Pretty;
 pub use build::comp_time_env_rev;
+pub use pretty::Pretty;
+
+#[cfg(debug_assertions)]
+const LOG_LEVEL: &str = "debug";
+#[cfg(not(debug_assertions))]
+const LOG_LEVEL: &str = "info";
+
+pub fn init_env_logger() {
+	env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(LOG_LEVEL)).init();
+}
